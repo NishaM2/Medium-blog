a168509482309d2f1ab3cef6fd6a6174e6c1164f
NishaM2 <nisham2522@gmail.com>
2025-08-07 00:12:54 +0530

    done

---
+++ frontend/src/hooks/index.ts
@@ -0,0 +1,58 @@
+import { useEffect, useState } from "react";
+import { BACKEND_URL } from "../config";
+import axios from "axios";
+
+ export interface Blog {
+    [x: string]: any;
+    content: string;
+    title: string;
+    id: string;
+    author: {
+        name: string;
+    }
+}
+
+export const useBlog = ({ id }: {id: string}) => {
+    const [blog, setBlog] = useState<Blog>();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/api/v1/blog/${id}` , {
+            headers: {
+                Authorization: localStorage.getItem("token")
+            }
+        })
+            .then(response => {
+                setBlog(response.data.blog);
+                setLoading(false);
+            })
+    }, [id])
+
+    return {
+        loading,
+        blog
+    }
+}
+
+
+export const useBlogs = () => {
+    const [blogs, setBlogs] = useState<Blog>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get(`${BACKEND_URL}/api/v1/blog/bulk` , {
+            headers: {
+                Authorization: localStorage.getItem("token")
+            }
+        })
+            .then(response => {
+                setBlogs(response.data.blogs);
+                setLoading(false);
+            })
+    }, [])
+
+    return {
+        loading,
+        blogs
+    }
+}
---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,38 @@
+import { Appbar } from "../component/Appbar"
+import { BlogCard } from "../component/BlogCard"
+import { BlogSkeleton } from "../component/BlogSkeleton";
+import { useBlogs } from "../hooks"
+
+export const Blogs = () => {
+    const { loading, blogs } = useBlogs();
+
+    if(loading) {
+        return <div>
+            <Appbar />
+            <div className="flex justify-center">
+                <div>
+                    <BlogSkeleton />
+                    <BlogSkeleton/>
+                    <BlogSkeleton/>
+                    <BlogSkeleton/>
+                    <BlogSkeleton/>
+                    <BlogSkeleton/>
+                </div>
+            </div>
+        </div>
+    }
+    return <div>
+        <Appbar/>
+        <div className="flex justify-center">
+            <div className="max-w-xl">
+                {blogs.map(blog => <BlogCard
+                   id={blog.id}
+                   authorName={blog.author.name || "Anonymous"}
+                   title={blog.title}
+                   content={blog.content}
+                   publishedDate={"8th august 2025"}
+                />)}
+            </div>
+        </div>
+    </div>
+}
